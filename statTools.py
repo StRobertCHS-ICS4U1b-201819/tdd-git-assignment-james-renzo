fd75ce8d3602226e38b48aea713c5a14ce19be41
robuntu <robuntu@stro.ycdsb.ca>
2018-11-07 10:52:58 -0500

    Complete the 'mean' section of TDD. Testing included 'basic', 'unusual', and 'exhaustive'.

--- statTools.py
+++ statTools.py
@@ -1,1 +1,6 @@
+def mean(x):
+    if x == "Illegal Input":
+        return "None"
+    else:
+        return round(sum(x) / len(x), 2)
 
--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,17 @@
+import pytest
+from statTools import *
 
+def test_mean_basic():
+    assert(mean([1,2,3,4,5]) == 3)
+
+def test_mean_zero():
+    assert(mean([0]) == 0)
+
+def test_mean_longAndLarge():
+    assert(mean([10,20,30,40,50,60,70,80,90,100]) == 55)
+
+def test_mean_decimal():
+    assert(mean([1.5,1.2,3.4,5.6]) == 2.92)
+
+def test_mean_illegal():
+    assert(mean("Illegal Input") == "None")
