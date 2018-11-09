6215033bf99cf526a02d7350c21d812a1325c24d
robuntu <robuntu@stro.ycdsb.ca>
2018-11-09 11:08:43 -0500

    import packages

--- statTools.py
+++ statTools.py
@@ -7,3 +7,25 @@ def mean(myList):
          sum += myList[i]
       avg = sum / len(myList)
       return avg
+
+def range(theList):
+    if len(theList) == 0:
+        return 0
+    else:
+        total = 0
+        for t in range(len(theList)):
+                total += theList[t]
+        high = max(len(theList))
+    return high
+
+def deviant(myList):
+   if len(myList) == 0:
+      return 0
+   else:
+      sum = 0
+      for i in range(len(myList)):
+         sum += myList[i]
+      men = sum / len(myList)
+      sqrt = 2 ** (sum / len(myList))
+      avg = sqrt
+      return avg
--- test_statTools.py
+++ test_statTools.py
@@ -3,3 +3,13 @@ from statTools import *
 
 def test_variance_basic1():
     assert(mean([3*2+4*2+5*2+6*2+7*2]))
+
+def test_range_basic0():
+    assert(range([9, 4, 6, 7, 8]) == 9)
+
+def test_range_basic1():
+    assert(range([0]) == 0)
+
+def test_range_basic_large3():
+    assert(range([80, 90, 100, 190]) == 190)
+
