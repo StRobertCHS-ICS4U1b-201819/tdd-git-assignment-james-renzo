a9487408c25a4adc0c8cecf5bede3d8d8004bb93
jamesl0 <james.lau@cyberdude.com>
2018-11-08 21:34:51 -0500

    Can't figure out my mean function, how to take in an illegal parameter. Starting to do median function, make and pass first test.

--- statTools.py
+++ statTools.py
@@ -1,6 +1,8 @@
 def mean(x):
-    if x == "Illegal Input":
-        return "None"
+    if x == str:
+        return None
     else:
         return round(sum(x) / len(x), 2)
 
+def median(x):
+    return 3
