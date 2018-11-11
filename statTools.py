b7b7460166d5a07cc0633187a5c4d4f6d898e6de
jamesl0 <james.lau@cyberdude.com>
2018-11-11 15:42:57 -0500

    Complete "illegal", and "decimal" test for range function. One more test, the "zero" test incoming before I push and merge in to master.

--- statTools.py
+++ statTools.py
@@ -1,26 +1,32 @@
 def mean(x):
     if x == str(x):
-        return "None"
+        return "Illegal parameter, please input a number"
     else:
         return round(sum(x) / len(x), 2)
 
 def median(x):
-    x.sort()
-    mid = (0 + len(x)) // 2
-    Avg = x[mid]
-    if len(x) % 2 == 0:
-        return x[Avg-1]
+    if x == str(x):
+        return "Illegal parameter, please input a number"
     else:
-        return Avg
+        x.sort()
+        mid = (0 + len(x)) // 2
+        Avg = x[mid]
+        if len(x) % 2 == 0:
+            return x[Avg-1]
+        else:
+            return Avg
 
 def mode(x):
     if x == str(x):
-        return "None"
+        return "Illegal parameter, please input a number"
     else:
         Avg = max(set(x),key=x.count)
         return Avg
 
 def range(x):
-    minNum = min(x)
-    maxNum = max(x)
-    return maxNum - minNum
+    if x == str(x):
+        return "Illegal parameter, please input a number"
+    else:
+        minNum = min(x)
+        maxNum = max(x)
+        return maxNum - minNum
--- test_statTools.py
+++ test_statTools.py
@@ -14,7 +14,7 @@ def test_mean_decimal():
     assert(mean([1.5, 1.2, 3.4, 5.6]) == 2.92)
 
 def test_mean_illegal():
-    assert(mean("Illegal Input") == "None")
+    assert(mean("Illegal Input") == "Illegal parameter, please input a number")
 
 def test_median_basic():
     assert(median([1,2,3,4,5]) == 3)
@@ -32,7 +32,7 @@ def test_median_decimalAndLong():
     assert(median([3.1,3.2,3.3,3.4,3.6,3.7,3.8,3.9,4.0]) == 3.6)
 
 def test_median_illegal():
-    assert(mean("Illegal Input") == "None")
+    assert(mean("Illegal Input") == "Illegal parameter, please input a number")
 
 def test_mode_basic():
     assert(mode([1,2,2,3,4]) == 2)
@@ -50,7 +50,7 @@ def test_mode_decimalAndLong():
     assert(mode([0.1,0.2,0.3,0.5,0.8,0.4,1.1,1.1,2.3,3.4,5.5,1.1]) == 1.1)
 
 def test_mode_illegal():
-    assert(mode("Illegal Input") == "None")
+    assert(mode("Illegal Input") == "Illegal parameter, please input a number")
 
 def test_range_basic():
     assert(range([1,2,3,4,5]) == 4)
@@ -58,3 +58,8 @@ def test_range_basic():
 def test_range_basic2():
     assert(range([2,3,4,5,7]) == 5)
 
+def test_range_illegal():
+    assert(range("Illegal Input") == "Illegal parameter, please input a number")
+
+def test_range_decimal():
+    assert(range([1.2,1.3,1.1,1.4,1.5,1.6]) == 0.5)
