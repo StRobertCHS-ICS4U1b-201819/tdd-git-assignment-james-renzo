bd3b04103e8cb53ea4c183a32243a2c944d164f7
RTtenedero <36413776+RTtenedero@users.noreply.github.com>
2018-11-13 10:06:15 -0500

    Update statTools.py

--- statTools.py
+++ statTools.py
@@ -24,8 +24,21 @@ def deviant(myList):
    else:
       sum = 0
       for i in range(len(myList)):
-         sum += myList[i]
-      men = sum / len(myList)
-      sqrt = 2 ** (sum / len(myList))
+        sum += myList[i]
+        men = sum / len(myList)
+        for i in range(len(myList)):
+            diff = myList[i] - men
+            vari = diff*2/len(myList)
+      sqrt = 2 ** vari
       avg = sqrt
       return avg
+
+def quartile(List):
+    if len(List) == 0:
+        return 0
+    else:
+        sum = 0
+        for r in range(len(List)):
+            sum += List[r]
+            medi = sum / 2
+            quar = medi / 2
