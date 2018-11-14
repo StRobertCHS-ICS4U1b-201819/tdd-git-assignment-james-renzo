80c48ba21df3076835b4960485a834e88ac64073
RTtenedero <36413776+RTtenedero@users.noreply.github.com>
2018-11-13 22:11:38 -0500

    Update test_statTools.py

--- test_statTools.py
+++ test_statTools.py
@@ -25,4 +25,23 @@ def test_deviance_basic1():
 def test_deviance_large1():
     assert(deviant([100, 99, 64, 190, 435]))
     
-def test_
+def test_lowerQuartile_0():
+    assert(lowQuartile([1, 2, 3, 4, 5, 6, 7]) == 2)
+    
+def test_lowerQuartile_illegal():
+    assert(lowQuartile(["This is pie"]) == "error")
+    
+def test_lowerQuartile_1():
+    assert(lowQuartile([0]) == 0)
+
+def test_lowerQuartile_2():
+    assert(lowQuartile([0, 1, 2, 3]) == 0.5)
+    
+def test_lowerQuartile_3():
+    assert(lowQuartile([0, 6, 7, 10, 43, 54, 65, 67, 87, 89]) == 7)
+    
+def test_higherQuartile_0():
+    assert(highQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == 0)
+    
+def test_higherQuartile_illegal():
+    assert(highQuartile(["quarter"]) == "error")
